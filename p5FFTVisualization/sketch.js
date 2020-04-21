443db67badffec35561b42dee6d1176e2745e4b9
Parth Shah <parthnay@buffalo.edu>
2020-04-21 09:35:43 -0400

    changes ver1

--- p5FFTVisualization/sketch.js
+++ p5FFTVisualization/sketch.js
@@ -4,13 +4,14 @@ var button;
 
 var w;
 var bands;
+var curr;
 
 function setup() {
   // put setup code here
   createCanvas(600 ,400)
   bands = 64;
   mic = new p5.AudioIn();
-  colorMode(HSB)
+  // colorMode(HSB)
   button = createButton("Start Mic");
   button.mousePressed(micOn);
   fft = new p5.FFT(0.85,bands);
@@ -34,6 +35,7 @@ function micOn() {
 
 function draw() {
   background(0)
+  // curr = mic.getLevel();
   var spectrum = fft.analyze();
   var spectrumRev = spectrum.slice().reverse();
   var finalSpectrum = spectrumRev.concat(spectrum);
@@ -41,12 +43,14 @@ function draw() {
   // noStroke();
   //color -  #D2EDD4 ---- prog - '#46B54D'
   stroke(255)
+  // fill(210,237,212)
+  fill(64,64,64);
+  // console.log(mic.getLevel());
   for (let i = 0; i < finalSpectrum.length; i++){
     var amp = finalSpectrum[i]*0.75;
-    if(i > 50 || i < 80){
-      amp *= 0.5;
-    }
-    fill((i*2)%255,255,255);
+    // if(i > 50 || i < 80){
+    //   amp *= 0.5;
+    // }
     var x = map(i, 0, finalSpectrum.length, 0, width - w);
     var y = map(amp, 0, 256, 0, height/1.5)
     rect(i*w, (height - y)/2, w, y)
